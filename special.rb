7b46f3f361564bb1d074e633f14bced047441445
phuong089 <melesomsenynzge@hotmail.com>
2025-03-18 14:17:52 +0800

    style[core]: format code in special.rb

--- special.rb
+++ special.rb
@@ -1,1 +1,9 @@
-puts "Hello, World!"
+# special.rb
+# This script prints a greeting message to the console.
+
+def greet
+  puts "Hello, World!"
+end
+
+# Call the greet method to display the message
+greet
